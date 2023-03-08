bcd078610a98a2e8e058bdb6085f9276ec3c06e1
yoyyii <114526999+yoyyii@users.noreply.github.com>
2023-03-08 12:38:08 -0800

    Add files via upload

--- Music Box/View.js
+++ Music Box/View.js
@@ -1,16 +1,23 @@
 function View(canvas) {
     this.canvas = canvas;
     this.clicks = [];
+    this.frameRate = 1000 / 30;
+    this.loopRate = 4000;
+    this.maxRadius = 80;
 }
 
 View.prototype.handleClick = function(event) {
     var view = this;
     var x = event.offsetX;
     var y = event.offsetY;
+    var pos = view.clicks.push({x: x, y: y, radius: 0});
+    Audio.play(x % 10);
 
-    var pos = view.clicks.push({x : x, y : y, radius : 100});
-    console.log("Add a circle at ", x, ", ", y);
-}
+    setInterval(function() {
+        view.clicks[pos-1].radius = 0;
+        Audio.play(x % 10);
+    }, view.loopRate);
+};
 
 View.prototype.updateDisplay = function() {
     var view = this;
@@ -18,13 +25,28 @@ View.prototype.updateDisplay = function() {
     context.clearRect(0, 0, view.canvas.width, view.canvas.height);
     context.fillStyle = 'black';
     context.fillRect(0, 0, view.canvas.width, view.canvas.height);
-    view.drawCircle(context, 150, 150, 100, 1);
-    
+
+    for (var i = 0; i < view.clicks.length; i++) {
+        var circle = view.clicks[i];
+
+        if (circle.radius > view.maxRadius) {
+            continue;
+        }
+
+        circle.radius += 1;
+        var alpha = 0.7;
+
+        if (circle.radius > (view.maxRadius - 15)) {
+            alpha = (view.maxRadius - circle.radius) / 10;
+        }
+        
+        view.drawCircle(context, circle.x, circle.y, circle.radius, alpha);
+    }
 };
-    
+
 View.prototype.drawCircle = function(context, x, y, radius, alpha) {
     context.beginPath();
     context.arc(x, y, radius, 0, 2*Math.PI);
-    context.fillStyle = "rgba(" + x%256 + ", " + y%256 + ", " + (x * y % 256)+ " ," + alpha + ")";
+    context.fillStyle = "rgba(" + x % 256 + ", " + y % 256 + ", " + (x * y % 256)+ " ," + alpha + ")";
     context.fill();
 };
--- Music Box/music box.js
+++ Music Box/music box.js
@@ -1,12 +1,18 @@
 window.onload = function() {
-    var canvas = document.getElementById('canvas');
-    var view = new View(canvas);
-    view.updateDisplay();
-}
+    var bufferLoader = new BufferLoader(
+        Audio.audioContext,
+        ["A4.mp3", "A5.mp3", "C4.mp3", "C5.mp3", "D4.mp3", "D5.mp3", "E4.mp3", "E5.mp3",
+        "G4.mp3", "G5.mp3" ],
+        finishedLoading
+    );
 
-window.onload = function() {
-    var canvas = document.getElementById('canvas');
-    var view = new View(canvas);
-    canvas.addEventListener("click", view.handleClick.bind(view), false);
-    view.updateDisplay();
+    bufferLoader.load();
+
+    function finishedLoading(bufferList) {
+        Audio.init(bufferList);
+        var canvas = document.getElementById("canvas");
+        var view = new View(canvas);
+        canvas.addEventListener("mousedown", view.handleClick.bind(view), false);
+        setInterval(view.updateDisplay.bind(view), view.frameRate);
+    }
 };
